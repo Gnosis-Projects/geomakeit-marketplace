c49132a2b03a96494ec5c251659c3970c679c1da
unknown <verikokkos.aristidis@gmail.com>
2023-05-24 10:53:33 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppListComponent } from './app-list/app-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppListComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +19,11 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const appListElement = createCustomElement(AppListComponent, { injector: this.injector });
+    customElements.define('app-list', appListElement);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+// Bootstrap as a custom element
+import './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
