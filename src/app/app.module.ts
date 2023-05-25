c98cd3d97d8371bec3335fdaefcd32d3a37af3e3
Vasileios Georgoulas <b_gvas@yahoo.gr>
2023-05-25 20:05:55 +0300

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,33 @@
 import { DoBootstrap, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppListComponent } from './app-list/app-list.component';
+import {PlayStoreModule} from "./modules/play-store.module";
+import {HomeComponent} from "./modules/home/home.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppListComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PlayStoreModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule implements DoBootstrap {
   constructor(private injector: Injector) {}
 
   ngDoBootstrap() {
-    const appListElement = createCustomElement(AppListComponent, { injector: this.injector });
-    customElements.define('app-list', appListElement);
+    const appListElement = createCustomElement(HomeComponent, { injector: this.injector });
+    customElements.define('play-store', appListElement);
   }
 }
