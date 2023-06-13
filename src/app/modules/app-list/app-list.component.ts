4169362efc2d87f6622c8425ad20f08712a738bc
Vasileios Georgoulas <b_gvas@yahoo.gr>
2023-06-13 23:40:10 +0300

    Changes in appList subscription

--- src/app/modules/app-list/app-list.component.ts
+++ src/app/modules/app-list/app-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { SelectorService } from 'src/app/services/selector.service';
-import { Game_List } from 'src/models/interfaces/game-list.interface';
 import { GetGamesService } from 'src/app/services/get-games.service';
+import {map, Observable} from "rxjs";
+import {GameDetails} from "../../../models/interfaces/game-details.interface";
 
 @Component({
   selector: 'app-app-list',
@@ -12,12 +13,11 @@ export class AppListComponent implements OnInit {
 
   @Output() showListChange = new EventEmitter<boolean>();
   test: any;
-  games: Game_List[] = [];
+  games$: Observable<GameDetails[]>;
   showList: boolean = true;
-  categories!: string[];
+  categories$: Observable<string[]>;
   selectedTabIndex!: number;
   activeCategory: string = '';
-
   categoriesState: { [key: string]: boolean } = {};
 
   constructor(private appService: SelectorService,private gameService: GetGamesService) {
@@ -37,11 +37,14 @@ export class AppListComponent implements OnInit {
 
   ngOnInit(): void {
     this.showList = true;
-    this.categories = [...new Set(this.games.map((game: Game_List) => game.category))] as string[];
-    this.categories.push('All'); 
+    this.getAllGames();
+    this.getCategories();
+    /*this.categories = [...new Set(this.games.map((game: Game_List) => game.category))] as string[];
+    this.categories.push('All');*/
     this.selectedTabIndex = 0;
+
     // testing the services here to see wtf im doing
-    this.gameService.getGames().subscribe({
+   /* this.gameService.getGames().subscribe({
       next: (data: Object) => {
         this.games = data as Game_List[];
         // parse the screenshots property from string to array
@@ -55,12 +58,22 @@ export class AppListComponent implements OnInit {
       },
       complete: () => {
         console.log("ektupwnw ta paixnidia apo to get")
-        console.log(this.games)      
+        console.log(this.games)
       }
-    });
-    
+    });*/
 
+  }
 
+  getAllGames() {
+    this.games$ = this.gameService.getGames();
+  }
+
+  getCategories() {
+    this.categories$ = this.games$.pipe(map(games => {
+      const categoriesArray: string[] = [];
+      games.map(game => categoriesArray.push(JSON.parse(game.category || '')))
+      return categoriesArray;
+    }))
   }
 
   selectApp(app: any) {
