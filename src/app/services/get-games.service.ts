706c368c3e8a4b8378488a537b4685d3697c16ec
Vasileios Georgoulas <b_gvas@yahoo.gr>
2023-06-13 23:30:36 +0300

    Changes in GameService

--- src/app/services/get-games.service.ts
+++ src/app/services/get-games.service.ts
@@ -1,21 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Observable, throwError} from "rxjs";
+import {GameDetails} from "../../models/interfaces/game-details.interface";
+import {catchError} from "rxjs/operators";
+import {ToastrService} from "ngx-toastr";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetGamesService {
 
-  constructor(private http: HttpClient) { }
+  baseUrl = '/api/StudioComm';
 
-  getGames() {
-    const url = '/api/StudioComm/List';
-    return this.http.get(url);
+  constructor(private http: HttpClient, private toastrService: ToastrService) { }
+
+  getGames(): Observable<GameDetails[]> {
+    return this.http.get(this.baseUrl + '/List')
+      .pipe(catchError(error => {
+        this.toastrService.error('Error while fetching list of games!');
+        return error;
+    }));
   }
+
+
   getGameDetails(id: number) {
-    const url = `/api/StudioComm/GetGameByID/${id}`;
-    return this.http.get(url);
+    return this.http.get(this.baseUrl + '/GetGameByID/' + id);
   }
-  
-  
+
+
 }
