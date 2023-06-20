761ce4480c6a02a1851fceac2b5a3e867652dc51
Vasileios Georgoulas <b_gvas@yahoo.gr>
2023-06-20 20:30:51 +0300

    Changes in error-interceptor to be able to catch different errors

--- src/app/modules/searchbar/searchbar.component.ts
+++ src/app/modules/searchbar/searchbar.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit, Directive, Output, EventEmitter, ElementRef, HostListener, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Directive,
+  Output,
+  EventEmitter,
+  ElementRef,
+  HostListener,
+  ViewChild,
+  OnDestroy
+} from '@angular/core';
 import { Game_List } from 'src/models/interfaces/game-list.interface';
 import { SelectorService } from 'src/app/services/selector.service'; // import the service
 import { GetGamesService } from 'src/app/services/get-games.service';
-import { Subscription } from 'rxjs';
+import {Subject, Subscription, takeUntil} from 'rxjs';
 
 @Component({
  selector: 'app-searchbar',
  templateUrl: './searchbar.component.html',
  styleUrls: ['./searchbar.component.css']
 })
-export class SearchbarComponent implements OnInit {
+export class SearchbarComponent implements OnInit, OnDestroy {
 
  games!: Game_List[];
  searchValue = '';
  filteredApps: Game_List[] = [];
  showDropdown: boolean = false; // initial value
+ stop$: Subject<boolean> = new Subject<boolean>();
  private _selectedIndex: number = -1;
  @ViewChild('input') inputElement!: ElementRef;
 
@@ -23,74 +34,76 @@ export class SearchbarComponent implements OnInit {
  constructor(private selectorService: SelectorService,private getGamesService: GetGamesService) { }
 
  ngOnInit(): void {
-  this.getGamesService.getGames().subscribe(games => {
-  this.games = games;
-  this.filteredApps = games;
-});
+    this.getGamesService.getGames().pipe(takeUntil(this.stop$)).subscribe(games => {
+        this.games = games;
+        this.filteredApps = games;
+    });
  }
- ngOnDestroy(): void {
-  console.log("hey bro i unsubed in the searchbar");
-  this.subscription.unsubscribe();
-}
 
- filterApps() {
-  // filter the games by name
-  this.filteredApps = this.games.filter(game => game.title.toLowerCase().includes(this.searchValue.toLowerCase()));
-  // sort the filtered games by match score
-  this.filteredApps.sort((a, b) => {
-    // get the match score for each app
-    let scoreA = this.getMatchScore(a.title.toLowerCase(), this.searchValue.toLowerCase());
-    let scoreB = this.getMatchScore(b.title.toLowerCase(), this.searchValue.toLowerCase());
-    // compare the scores
-    if (scoreA > scoreB) {
-      return -1; // app A has a higher score, so it comes before app B
-    } else if (scoreA < scoreB) {
-      return 1; // app B has a higher score, so it comes before app A
-    } else {
-      return 0; // app A and B have the same score, so they stay in the same order
-    }
-  });
-}
+  ngOnDestroy(): void {
+    console.log("hey bro i unsubed in the searchbar");
+    this.subscription.unsubscribe();
+    this.stop$.next(true);
+    this.stop$.unsubscribe();
+  }
 
-getMatchScore(name: string, value: string): number {
- if (name.startsWith(value)) {
- return 100;
- }
- if (name.includes(value)) {
- return 50;
- }
- return 0;
-}
+  filterApps() {
+    // filter the games by name
+    this.filteredApps = this.games.filter(game => game.title.toLowerCase().includes(this.searchValue.toLowerCase()));
+    // sort the filtered games by match score
+    this.filteredApps.sort((a, b) => {
+      // get the match score for each app
+      let scoreA = this.getMatchScore(a.title.toLowerCase(), this.searchValue.toLowerCase());
+      let scoreB = this.getMatchScore(b.title.toLowerCase(), this.searchValue.toLowerCase());
+      // compare the scores
+      if (scoreA > scoreB) {
+        return -1; // app A has a higher score, so it comes before app B
+      } else if (scoreA < scoreB) {
+        return 1; // app B has a higher score, so it comes before app A
+      } else {
+        return 0; // app A and B have the same score, so they stay in the same order
+      }
+    });
+  }
 
-onAppClick(app: Game_List, event: Event) {
-  console.log("click")
-  event.preventDefault();
-  this.selectorService.selectApp(app.game_id);
-  this.selectorService.setShowList(false);
-  this.searchValue = app.title;
-  this.onBlur();
-}
+  getMatchScore(name: string, value: string): number {
+     if (name.startsWith(value)) {
+     return 100;
+     }
+     if (name.includes(value)) {
+     return 50;
+     }
+     return 0;
+  }
 
-onBlur() {
-    this.showDropdown = false;
+  onAppClick(app: Game_List, event: Event) {
+    console.log("click")
+    event.preventDefault();
+    this.selectorService.selectApp(app.game_id);
+    this.selectorService.setShowList(false);
+    this.searchValue = app.title;
+    this.onBlur();
+  }
 
-}
- 
-clearSearch() {
-  this.searchValue = ''; 
-  this.showDropdown = false;
-}
+  onBlur() {
+      this.showDropdown = false;
+  }
 
-onChange() {
-  // update the showDropdown property based on the input value
-  if (this.searchValue.trim()) {
-    this.showDropdown = true;
-  } else {
+  clearSearch() {
+    this.searchValue = '';
     this.showDropdown = false;
   }
-  // reset the selectedIndex to -1
-  this.selectedIndex = -1;
-}
+
+  onChange() {
+    // update the showDropdown property based on the input value
+    if (this.searchValue.trim()) {
+      this.showDropdown = true;
+    } else {
+      this.showDropdown = false;
+    }
+    // reset the selectedIndex to -1
+    this.selectedIndex = -1;
+  }
 
 onKeydown(event: KeyboardEvent) {
   // handle the arrow keys, the enter key and the escape key
@@ -124,18 +137,18 @@ onKeydown(event: KeyboardEvent) {
   }
 }
 
-// use a setter and a getter for the selectedIndex property
-get selectedIndex(): number {
-  return this._selectedIndex;
-}
-
-set selectedIndex(value: number) {
-  // update the private variable
-  this._selectedIndex = value;
-  // update the input value with the selected option if it exists
-  if (value > -1 && value < this.filteredApps.length) {
-    let app = this.filteredApps[value];
-    this.searchValue = app.title;
+  // use a setter and a getter for the selectedIndex property
+  get selectedIndex(): number {
+    return this._selectedIndex;
   }
-}
+
+  set selectedIndex(value: number) {
+    // update the private variable
+    this._selectedIndex = value;
+    // update the input value with the selected option if it exists
+    if (value > -1 && value < this.filteredApps.length) {
+      let app = this.filteredApps[value];
+      this.searchValue = app.title;
+    }
+  }
 }
